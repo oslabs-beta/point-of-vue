bb7e9da9a6db3a6ed7ad88eb1d35299b723ccfa2
wrobson5467 <will.robson@gmail.com>
2022-05-26 11:53:32 -0700

    basic file structure

---
+++ src/formatting.ts
@@ -0,0 +1,4 @@
+//  Devtool plugin API only accepts data in a certain format
+//     - Use Pinia codebase as an example
+//         - State will be imported to this file
+//             - State will be formatted here before exported to the main plugin file
---
+++ src/plugin.ts
@@ -0,0 +1,8 @@
+//     - Initial setup and registration of our dev tool
+//     - Where we initialize our timeline layers
+//     - Initialize our state panel
+//     - Populate our timeline
+//     - Import the formatted state from our format file
+//     - Populate our state panel with this info
+
+// setupDevtoolsPlugin function
